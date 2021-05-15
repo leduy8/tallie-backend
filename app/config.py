51fb62a1af6e548bbca1aaa7ef0530fff68e1d70
duy.leduc8 <duy.leduc8@gmail.com>
2021-05-15 15:49:49 +0700

    add user and product model, connected to postgreSQL

--- app/config.py
+++ app/config.py
@@ -1,4 +1,11 @@
 import os
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Tallie1234'
+
+    # ? For sqlite
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'app.db')
+    # ? For postgreSQL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:123456@localhost/Tallie'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ app/models.py
@@ -0,0 +1,38 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(50), index=True, unique=True)
+    phone = db.Column(db.String(10))
+    address = db.Column(db.String(150))
+    bio = db.Column(db.String(250))
+    is_seller = db.Column(db.Boolean())
+    products = db.relationship('product', backref='user_id', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self) -> str:
+        return f'<User {self.username}>'
+
+
+class Product(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    price = db.Column(db.Float(precision=2))
+    description = db.Column(db.String(80))
+    quantity = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return f'<Product {self.name}>'
