9edf39377d5d63d135fc053e6fffbb71c262dd0e
duy.leduc8 <duy.leduc8@gmail.com>
2021-06-19 09:54:26 +0700

    impl product api

--- app/api/products.py
+++ app/api/products.py
@@ -1,9 +1,9 @@
 from flask import jsonify, request, current_app
-from sqlalchemy import or_, and_
+from sqlalchemy import or_, and_, func
 from app import db
 from app.api import bp
 from .errors import bad_request, not_found
-from ..models import Product, Review
+from ..models import Product, Review, Seen
 
 
 def get_price_range(price_range):
@@ -24,7 +24,26 @@ def get_price_range(price_range):
 
 @bp.route('/products/featured')
 def featured_products():
-    pass
+    # ? Sort by review's stars of each product
+    page = request.args.get('page', 1, type=int)
+    featured = db.session.query(Product, func.sum(Review.star)).join(Product).filter(
+        Review.product_id==Product.id).group_by(Product.id).order_by(func.sum(Review.star).desc()).paginate(
+            page, current_app.config['PRODUCTS_PER_PAGE'], False
+        )
+    next_page = page=featured.next_num if featured.has_next else None
+    prev_page = page=featured.prev_num if featured.has_prev else None
+
+    return jsonify({
+        'data': [product_count[0].get_product_info() for product_count in featured.items],
+        '_meta': {
+            'page': page or 1,
+            'per_page': current_app.config['PRODUCTS_PER_PAGE'],
+            'total_pages': featured.pages,
+            'total_items': featured.total,
+            'next_page': next_page,
+            'prev_page': prev_page
+        }
+    })
 
 
 @bp.route('/products/best_selling_this_month')
@@ -34,7 +53,28 @@ def best_selling_products():
 
 @bp.route('/products/most_viewed')
 def most_viewed_products():
-    pass
+    # ? db.session.execute('SELECT product, count(seen.product_id) FROM product LEFT JOIN seen ON product.id = seen.product_id GROUP BY product ORDER BY count(seen.product_id) DESC')
+
+    page = request.args.get('page', 1, type=int)
+    products_counts = db.session.query(Product, func.count(Seen.product_id)).join(Product).filter(
+        Seen.product_id == Product.id).group_by(Product.id).order_by(func.count(Seen.product_id).desc()).paginate(
+            page, current_app.config['PRODUCTS_PER_PAGE'], False
+        )
+    next_page = page=products_counts.next_num if products_counts.has_next else None
+    prev_page = page=products_counts.prev_num if products_counts.has_prev else None
+
+    return jsonify({
+        'data': [{'product': product_count[0].get_product_info(), 'count': product_count[1]} for product_count in products_counts.items],
+        '_meta': {
+            'page': page or 1,
+            'per_page': current_app.config['PRODUCTS_PER_PAGE'],
+            'total_pages': products_counts.pages,
+            'total_items': products_counts.total,
+            'next_page': next_page,
+            'prev_page': prev_page
+        }
+    })
+
 
 
 @bp.route('/products/search')
