e2d19d888911870e5166e7dcfc2ac652e7730b32
duy.leduc8 <duy.leduc8@gmail.com>
2021-06-20 23:54:20 +0700

    revert to heroku postgre

--- app/config.py
+++ app/config.py
@@ -15,11 +15,11 @@ class Config(object):
     # ? For sqlite
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(base_dir, 'app.db')
     # ? For postgreSQL
-    # uri = os.getenv("DATABASE_URL")
-    # if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql://", 1)
-    # SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:123456@localhost/Tallie'
+    uri = os.getenv("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:123456@localhost/Tallie'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     IMAGE_SERVICE_URL = 'https://tallie-image.herokuapp.com'
